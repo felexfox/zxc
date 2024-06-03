74cc47e650184cd787b5765c39180f350f292390
just_lofe <97516390+justlofe@users.noreply.github.com>
2024-06-03 18:45:05 +0700

    fix

--- src/main/java/org/flx/zxc/Eventsformdr/CulturesBreakEvent.java
+++ src/main/java/org/flx/zxc/Eventsformdr/CulturesBreakEvent.java
@@ -18,7 +18,7 @@ public class CulturesBreakEvent implements Listener {
             bbe.getPlayer().sendMessage(mm.deserialize("● <gradient:#FFFFFF:#BCBAB8><b> Modoru</gradient>"+": Вы сломали пшеницу! Вы так же можете просто её <underlined><bold>собрать</underlined>" + " нажав ПКМ по любой культуре!" + "<click:open_url:https://wiki.modoru.fun/other/crops><bold>Подробнее тут.</click>"));
             messageSent = true;
         }
-        else if (bbe.getBlock().getType().equals(Material.POTATO) && !messageSent) {
+        else if (bbe.getBlock().getType().equals(Material.POTATOES) && !messageSent) {
             bbe.getPlayer().sendMessage(mm.deserialize("● <gradient:#FFFFFF:#BCBAB8><b> Modoru</gradient>"+": Вы сломали картофель! Вы так же можете просто его <underlined><bold>собрать</underlined>" + " нажав ПКМ по любой культуре!" + "<click:open_url:https://wiki.modoru.fun/other/crops><bold>Подробнее тут.</click>"));
             messageSent = true;
         }
