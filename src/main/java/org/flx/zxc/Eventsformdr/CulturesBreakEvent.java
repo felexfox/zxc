a9550c4236edff7687df1f4e10ac79b5f9c4989a
felexfox <88285641+felexfox@users.noreply.github.com>
2024-06-03 14:35:35 +0300

    added WeatBreakEvent on plugin

---
+++ src/main/java/org/flx/zxc/Eventsformdr/CulturesBreakEvent.java
@@ -0,0 +1,34 @@
+package org.flx.zxc.Eventsformdr;
+
+import net.kyori.adventure.text.minimessage.MiniMessage;
+import org.bukkit.Material;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.block.BlockBreakEvent;
+
+public class CulturesBreakEvent implements Listener {
+
+    private boolean messageSent = false;
+
+    private static final MiniMessage mm = MiniMessage.miniMessage();
+
+    @EventHandler
+    public void BreakBlockEvent(BlockBreakEvent bbe){
+        if (bbe.getBlock().getType().equals(Material.WHEAT) && !messageSent) {
+            bbe.getPlayer().sendMessage(mm.deserialize("● <gradient:#FFFFFF:#BCBAB8><b> Modoru</gradient>"+": Вы сломали пшеницу! Вы так же можете просто её <underlined><bold>собрать</underlined>" + " нажав ПКМ по любой культуре!" + "<click:open_url:https://wiki.modoru.fun/other/crops><bold>Подробнее тут.</click>"));
+            messageSent = true;
+        }
+        else if (bbe.getBlock().getType().equals(Material.POTATO) && !messageSent) {
+            bbe.getPlayer().sendMessage(mm.deserialize("● <gradient:#FFFFFF:#BCBAB8><b> Modoru</gradient>"+": Вы сломали картофель! Вы так же можете просто его <underlined><bold>собрать</underlined>" + " нажав ПКМ по любой культуре!" + "<click:open_url:https://wiki.modoru.fun/other/crops><bold>Подробнее тут.</click>"));
+            messageSent = true;
+        }
+        else if (bbe.getBlock().getType().equals(Material.BEETROOTS) && !messageSent) {
+            bbe.getPlayer().sendMessage(mm.deserialize("● <gradient:#FFFFFF:#BCBAB8><b> Modoru</gradient>"+": Вы сломали свеклу! Вы так же можете просто её <underlined><bold>собрать</underlined>" + " нажав ПКМ по любой культуре!" + "<click:open_url:https://wiki.modoru.fun/other/crops><bold>Подробнее тут.</click>"));
+            messageSent = true;
+        }
+        else if (bbe.getBlock().getType().equals(Material.CARROTS) && !messageSent) {
+            bbe.getPlayer().sendMessage(mm.deserialize("● <gradient:#FFFFFF:#BCBAB8><b> Modoru</gradient>"+": Вы сломали морковь! Вы так же можете просто её <underlined><bold>собрать</underlined>" + " нажав ПКМ по любой культуре!" + "<click:open_url:https://wiki.modoru.fun/other/crops><bold>Подробнее тут.</click>"));
+            messageSent = true;
+        }
+    }
+}
--- src/main/java/org/flx/zxc/Zxc.java
+++ src/main/java/org/flx/zxc/Zxc.java
@@ -2,14 +2,14 @@ package org.flx.zxc;
 
 import org.bukkit.Bukkit;
 import org.bukkit.plugin.java.JavaPlugin;
-import org.flx.zxc.Eventsformdr.WheatBreakEvent;
+import org.flx.zxc.Eventsformdr.CulturesBreakEvent;
 
 public final class Zxc extends JavaPlugin {
 
     @Override
     public void onEnable() {
         Bukkit.getConsoleSender().sendMessage("[zxc] Plugin successfully loaded!");
-        getServer().getPluginManager().registerEvents(new WheatBreakEvent(), this);
+        getServer().getPluginManager().registerEvents(new CulturesBreakEvent(), this);
         Bukkit.getPluginManager().registerEvents(new EventHandler(), this);
 
     }
