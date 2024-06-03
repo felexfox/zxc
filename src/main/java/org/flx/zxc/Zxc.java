d831b080c43a7e83a7bb06ca793ee53904dae905
felexfox <88285641+felexfox@users.noreply.github.com>
2024-06-03 20:24:10 +0300

    added cultures break tip sound

--- src/main/java/org/flx/zxc/Zxc.java
+++ src/main/java/org/flx/zxc/Zxc.java
@@ -3,16 +3,26 @@ package org.flx.zxc;
 import org.bukkit.Bukkit;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.flx.zxc.Eventsformdr.CulturesBreakEvent;
+import org.flx.zxc.Eventsformdr.NoVillagersDiscount;
 import org.flx.zxc.Eventsformdr.SnowBreakEvent;
 
 public final class Zxc extends JavaPlugin {
+    private static Zxc instance;
+
+    public static Zxc getInstance() {
+        return instance;
+    }
 
     @Override
     public void onEnable() {
+
+        instance = this;
+
         Bukkit.getConsoleSender().sendMessage("[zxc] Plugin successfully loaded!");
 
         Bukkit.getPluginManager().registerEvents(new CulturesBreakEvent(), this);
         Bukkit.getPluginManager().registerEvents(new SnowBreakEvent(), this);
+        Bukkit.getPluginManager().registerEvents(new NoVillagersDiscount(), this);
     }
 
     @Override
